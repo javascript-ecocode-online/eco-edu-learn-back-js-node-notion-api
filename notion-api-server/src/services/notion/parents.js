1a1cdcc48f663608e2ca436e85966176698d62d1
mind7285 <javascript.ecocode.online@gmail.com>
2025-04-17 12:24:07 +0700

    Fix get parents

--- notion-api-server/src/services/notion/parents.js
+++ notion-api-server/src/services/notion/parents.js
@@ -6,13 +6,36 @@ export async function getAllParentPages(pageId) {
 
   while (true) {
     const page = await notion.pages.retrieve({ page_id: currentId });
+
+    // Lưu thông tin trang hiện tại
+    const currentInfo = {
+      id: currentId,
+      type: page.object,
+      title: extractTitle(page),
+    };
+    parents.push(currentInfo);
+
     if (page.parent?.type === 'page_id') {
       currentId = page.parent.page_id;
-      parents.push({ id: currentId });
     } else {
       break;
     }
   }
 
   return parents;
+}
+
+// 🔍 Trích xuất tiêu đề của page (nếu có)
+function extractTitle(page) {
+  if (!page.properties) return null;
+
+  const titleProp = Object.values(page.properties).find(
+    (prop) => prop.type === 'title'
+  );
+
+  if (titleProp && titleProp.title.length > 0) {
+    return titleProp.title.map(part => part.plain_text).join('');
+  }
+
+  return null;
 }
