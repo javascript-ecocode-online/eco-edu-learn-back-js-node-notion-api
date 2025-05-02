19eab4a530716ff1f930d778914c9b464cbceeca
mind7285 <javascript.ecocode.online@gmail.com>
2025-05-03 00:03:23 +0700

    .

--- notion-api-server/src/modules/eco/tasks/eco-task-url.js
+++ notion-api-server/src/modules/eco/tasks/eco-task-url.js
@@ -1,14 +1,14 @@
 export class EcoTaskUrl {
   static getEcoBuildUrl (cleanPageId) {
-    return `https://build.ecocode.online?id=${cleanPageId}`
+    return `https://notion-builder.ecocode.online?id=${cleanPageId}`
   }
 
   static getEcoLearnUrl (cleanPageId) {
-    return `https://learn.ecocode.online?id=${cleanPageId}`
+    return `https://notion-learn.ecocode.online?id=${cleanPageId}`
   }
 
   static getEcoTestUrl (cleanPageId) {
-    return `https://test.ecocode.online?id=${cleanPageId}`
+    return `https://notion-test.ecocode.online?id=${cleanPageId}`
   }
 }
 //eUrl
