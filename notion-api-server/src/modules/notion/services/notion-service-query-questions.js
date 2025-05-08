4914ef43165cd3841a94054fbcfeb5680222e2f7
mind7285 <javascript.ecocode.online@gmail.com>
2025-05-08 19:46:33 +0700

    Fix logs

--- notion-api-server/src/modules/notion/services/notion-service-query-questions.js
+++ notion-api-server/src/modules/notion/services/notion-service-query-questions.js
@@ -12,18 +12,20 @@ export class EcoNotionServiceQueryQuestions extends Base {
   async #eachLv2 (arr, b) {
     const me = this
     const id = me._cleanId(b.id)
-    arr.push(`----- 🥕 block: ----- ${id}`)
+    arr.push(`----- 🛸 block: ----- ${id}`)
     const reason = 'getPageWriteQuestion'
-    for (const rt of b.toggle.rich_text) {
-      arr.push(rt.plain_text)
+    const lv2Blocks = await me.getAllChildrenById(reason, id, 'toggle')
+    for (const b2 of lv2Blocks) {
+      //console.log(`🪽 -- lv2 id: ${b2.id} --`)
+      //arr.push(`🪽 -- lv2 id: ${b2.id} --`)
+      //console.log(`🪽 B2 id: ${b2.id}`)
 
-      const lv2Blocks = await me.getAllChildrenById(reason, id, 'toggle')
-      for (const b2 of lv2Blocks){
-        console.log(`🪽 B2 id: ${b2.id}`)
-        console.log(b2[b2.type].rich_text)
+      for (const rt of b2[b2.type].rich_text) {
+        arr.push(rt.plain_text)
+        console.log(rt)
       }
-      
     }
+    arr.push('')
   }
 
   async getPageWriteQuestion (reason, pageId, type) {
@@ -32,11 +34,11 @@ export class EcoNotionServiceQueryQuestions extends Base {
     const blocks = await me.getPageBlocks(reason, pageId)
     for (const b of blocks) {
       if (b.type === 'toggle') {
-        let isTarget = b[b.type].rich_text.some(rt =>
-          rt.plain_text.includes('🛫')
+        let isTarget = b[b.type].rich_text.some(
+          rt => rt.plain_text.includes('🛫') || rt.plain_text.includes('🪺')
         )
         if (isTarget) {
-          me.#eachLv2(arr, b)
+          await me.#eachLv2(arr, b)
 
           //console.log(b.toggle.rich_text)
         }
